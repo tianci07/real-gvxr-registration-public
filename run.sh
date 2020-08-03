c6e9c4f38afbadd829a6d83700f9ad5a2dbbda1e
effepivi <franck.p.vidal@gmail.com>
2020-08-03 14:57:21 +0100

    Update run.sh
    add overall result file

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
+echo "metrics,target_image,algorithm,downscale,pop_size,nb_generations,runtime_sec,parameters,ZNCC,SSIM,MI,GC,MAE,CS,SSD,GD" > results.csv
+
 for m in {ZNCC,SSIM,MI,GC,MAE,CS,SSD,GD};
 do
 	for i in {1..3};
@@ -11,6 +13,8 @@ do
 				  --downscale "$d"\
 				  --pop_size 200\
 				  --gen_size 200 > results-metrics$m-downscale$d-run$i.out 2> results-metrics$m-downscale$d-run$i.err
+				  
+		cat results-metrics$m-downscale$d-run$i.out >> results.csv
 		done
 	done
 done
